45b1b69e11daa1bfba8f2007edd375974a482110
Suyog0103 <131379044+Suyog0103@users.noreply.github.com>
2023-10-25 12:42:16 +0530

    Add files via upload

---
+++ WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Megalon.DAL; 
+using Megalon.Models; 
+ 
+public class CustomerController : Controller
+{
+    private readonly CustomerOrdersDB _context;
+
+    public CustomerController(CustomerOrdersDB context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public IActionResult CustomerTable()
+    {
+        List<Customer> customers = _context.Customers.ToList();
+        return View(customers);
+    }
+}
---
+++ WebApplication1/Controllers/OrdersController.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Megalon.DAL; 
+using Megalon.Models; 
+
+namespace WebApplication1.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly CustomerOrdersDB _context;
+
+        public OrdersController(CustomerOrdersDB context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetOrders(Guid customerId)
+        {
+            
+            List<Order> orders = _context.Orders
+                .Where(o => o.CustomerID == customerId)
+                .ToList();
+
+            
+            return View(orders);
+        }
+    }
+}
